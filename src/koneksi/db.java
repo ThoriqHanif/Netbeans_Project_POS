5bb05a2ef97f9b81122f6f484dd7156ea185cf15
thrq <thoriqmuh123.alkapro@gmail.com>
2023-09-11 05:32:28 +0700

    first commit

---
+++ src/koneksi/db.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class db {
+    
+    public static  Connection conn(){
+            
+        Connection con = null;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/pos","root","");
+            return con;
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+            return con;
+        }
+    }
+}
